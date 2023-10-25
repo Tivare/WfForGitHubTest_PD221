47fde91395ba030dc6435b4c9e6e623ff3cd688a
Khylko Andrii <hilko_a@itstep.academy>
2023-10-25 15:06:19 +0300

    Add random quotes generation

--- WfForGitHubTest_PD221/Form1.Designer.cs
+++ WfForGitHubTest_PD221/Form1.Designer.cs
@@ -29,27 +29,41 @@
         private void InitializeComponent()
         {
             this.lblData = new System.Windows.Forms.Label();
+            this.btnPush = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblData
             // 
             this.lblData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.lblData.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblData.Location = new System.Drawing.Point(163, 68);
+            this.lblData.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblData.Location = new System.Drawing.Point(161, 38);
             this.lblData.Name = "lblData";
-            this.lblData.Size = new System.Drawing.Size(423, 115);
+            this.lblData.Size = new System.Drawing.Size(433, 197);
             this.lblData.TabIndex = 0;
             this.lblData.Text = "Hello, World!!!";
             this.lblData.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPush
+            // 
+            this.btnPush.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPush.Location = new System.Drawing.Point(250, 284);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(238, 89);
+            this.btnPush.TabIndex = 1;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.btnPush);
             this.Controls.Add(this.lblData);
             this.Name = "Form1";
             this.Text = "Git Hub Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -57,5 +71,6 @@
         #endregion
 
         private Label lblData;
+        private Button btnPush;
     }
 }
--- WfForGitHubTest_PD221/Form1.cs
+++ WfForGitHubTest_PD221/Form1.cs
@@ -2,9 +2,31 @@ namespace WfForGitHubTest_PD221
 {
     public partial class Form1 : Form
     {
+        List<string> quots;
+        Random rand;
         public Form1()
         {
             InitializeComponent();
+            rand = new();
+           
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            lblData.Text = quots[rand.Next(quots.Count)];
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                quots = (await File.ReadAllLinesAsync("quotes.txt")).ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
         }
     }
 }
